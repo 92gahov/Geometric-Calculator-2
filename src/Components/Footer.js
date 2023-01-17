b2a6e6e2455a79885f7f9fcd2782a9f22a1a2e34
georgi-gahov <georgi.gahov@gmail.com>
2023-01-17 23:54:01 +0200

    React Geometric Calculator

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <div className="footer">
                 <h2>Geometric Calculator</h2>
                 <div className="author">
-                    <p>Created by <a href="https://www.facebook.com/profile.php?id=100000678950386" target="_blank" rel="noreferrer">G. Gahov</a></p>
+                    <p>Created by <a href="https://github.com/92gahov" target="_blank" rel="noreferrer">92gahov</a></p>
                 </div>
             </div>
         )
